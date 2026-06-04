6a3121a8e127983cdb3c7a8e4a2760f276cff584
Lalit <joshimohanlalit1303@gmail.com>
2026-06-05 02:37:05 +0530

    Update GA Measurement ID to new property

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
       <head>
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-KB14CWYVC1`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-TMM4LXTKJ2`}
         />
         <Script
           id="google-analytics"
@@ -76,7 +76,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-KB14CWYVC1');
+              gtag('config', 'G-TMM4LXTKJ2');
             `,
           }}
         />
